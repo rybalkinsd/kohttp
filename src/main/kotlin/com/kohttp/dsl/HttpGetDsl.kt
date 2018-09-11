4b1b24c58d320e03762a48ef6d42990b041afc74
Sergey Rybalkin <yan.brikl@gmail.com>
2018-09-11 17:48:18 +0300

    specified docs for all DSL methods (#14)

--- src/main/kotlin/com/kohttp/dsl/HttpGetDsl.kt
+++ src/main/kotlin/com/kohttp/dsl/HttpGetDsl.kt
@@ -5,11 +5,42 @@ import okhttp3.Call
 import okhttp3.Response
 
 /**
+ * Method provides an synchronous DSL call of HTTP GET
  *
- * @param client gives a possibility to provide your implementation of HttpClient
- * `CommonHttpClient` by default
+ * @return a `Response` instance.
  *
- * Created by Sergey on 22/07/2018.
+ * Usage example using the default `CommonHttpClient`:
+ *
+ *  <pre>
+ *  val response: Response? = httpGet {
+ *      host = "yourhost"
+ *      scheme = "https"
+ *      port = 8080
+ *      path = "path/to/resource"
+ *      param {
+ *          "your param" to "value"
+ *      }
+ *      header { ... }
+ *  }
+ *  response.use { ... }
+ *  </pre>
+ *
+ *  @param client allow to use your own implementation of HttpClient.
+ * `CommonHttpClient` is used by default.
+ *
+ * <pre>
+ *  val response: Response? = httpGet(customHttpClient) {
+ *      ...
+ *  }
+ * </pre>
+ *
+ * @see Response
+ * @see HttpContext
+ * @see ParamContext
+ * @see HeaderContext
+ * @see BodyContext
+ *
+ * Created by Sergey on 22/07/2018
  */
 fun httpGet(client: Call.Factory = CommonHttpClient, init: HttpGetContext.() -> Unit): Response {
     val context = HttpGetContext().apply(init)
--- src/main/kotlin/com/kohttp/dsl/HttpHeadDsl.kt
+++ src/main/kotlin/com/kohttp/dsl/HttpHeadDsl.kt
@@ -5,11 +5,41 @@ import okhttp3.Call
 import okhttp3.Response
 
 /**
+ * Method provides an synchronous DSL call of HTTP HEAD
  *
- * @param client gives a possibility to provide your implementation of HttpClient
- * `CommonHttpClient` by default
+ * @return a `Response` instance.
  *
- * Created by Sergey on 22/07/2018.
+ * Usage example using the default `CommonHttpClient`:
+ *
+ *  <pre>
+ *  val response: Response? = httpHead {
+ *      host = "yourhost"
+ *      scheme = "https"
+ *      port = 8080
+ *      path = "path/to/resource"
+ *      param {
+ *          "your param" to "value"
+ *      }
+ *      header { ... }
+ *  }
+ *  response.use { ... }
+ *  </pre>
+ *
+ *  @param client allow to use your own implementation of HttpClient.
+ * `CommonHttpClient` is used by default.
+ *
+ * <pre>
+ *  val response: Response? = httpHead(customHttpClient) {
+ *      ...
+ *  }
+ * </pre>
+ *
+ * @see Response
+ * @see HttpContext
+ * @see ParamContext
+ * @see HeaderContext
+ * @see BodyContext
+ *
  */
 fun httpHead(client: Call.Factory = CommonHttpClient, init: HttpHeadContext.() -> Unit): Response {
     val context = HttpHeadContext().apply(init)
