84cbad9d9fdccbf5bfe3b931a5a0a21bf121ea28
Sergey Rybalkin <yan.brikl@gmail.com>
2018-10-21 18:01:18 +0300

    http delete dsl (#27)

---
+++ src/test/kotlin/com/kohttp/dsl/HttpDeleteDslKtTest.kt
@@ -0,0 +1,69 @@
+package com.kohttp.dsl
+
+import org.junit.Test
+
+/**
+ * Created by Sergey on 23/07/2018.
+ */
+class HttpDeleteDslKtTest {
+
+    @Test
+    fun `delete request with form # postman echo`() {
+        httpDelete {
+            host = "postman-echo.com"
+            path = "/delete"
+
+            param {
+                "arg" to "iphone"
+            }
+
+            header {
+                "one" to 42
+                cookie {
+                    "aaa" to "bbb"
+                    "ccc" to 42
+                }
+            }
+
+            body {
+                form {
+                    "login" to "user"
+                    "email" to "john.doe@gmail.com"
+                }
+            }
+        }.use {
+            println(it.body()?.string())
+        }
+    }
+
+    @Test
+    fun `delete request with json # postman echo`() {
+        val response = httpDelete {
+            host = "postman-echo.com"
+            path = "/delete"
+
+            param {
+                "arg" to "iphone"
+            }
+
+            header {
+                "one" to 42
+                cookie {
+                    "aaa" to "bbb"
+                    "ccc" to 42
+                }
+            }
+
+            body {
+                json {
+                    "login" to "user"
+                    "email" to "john.doe@gmail.com"
+                }
+            }
+        }
+
+        response.use {
+            println(it.body()?.string())
+        }
+    }
+}
