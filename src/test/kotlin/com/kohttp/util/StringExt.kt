0742c6ccbd68028270164b6699b97894fc8d78bf
rybalkinsd <yan.brikl@gmail.com>
2018-10-26 06:23:18 +0300

    moving to really empty byte array

---
+++ src/test/kotlin/com/kohttp/util/StringExt.kt
@@ -0,0 +1,9 @@
+package com.kohttp.util
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+
+internal val stringMapper = ObjectMapper()
+
+fun String?.asJson() : JsonNode =
+    if(isNullOrBlank()) stringMapper.readTree("{}") else stringMapper.readTree(this)
