03eb40db1dfb5b36b4a8ea5ce0a48b878bffcbff
rybalkinsd <yan.brikl@gmail.com>
2018-12-07 13:00:13 +0300

    default client docs

--- src/main/kotlin/io/github/rybalkinsd/kohttp/client/DefaultHttpClient.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/client/DefaultHttpClient.kt
@@ -8,7 +8,25 @@ import java.util.concurrent.TimeUnit
 
 
 /**
- * Created by Sergey on 21/07/2018.
+ * A default http client.
+ * Default client is recommended to use for your requests.
+ *
+ * Default client is either configured with `/kohttp.yaml`
+ * or by defaults
+ *  connectTimeout: 10 seconds
+ *  readTimeout: 10 seconds
+ *  writeTimeout: 10 seconds
+ *  followRedirects: true
+ *  followSslRedirects: true
+ *
+ *  maxIdleConnections:  5
+ *  keepAliveDuration: 5 minutes
+ *
+ *
+ * For specific needs it's possible to fork `defaultHttpClient`
+ * @see `io.github.rybalkinsd.kohttp.client.OkHttpClientExtKt.fork`
+ *
+ * @author sergey 21/07/2018
  */
 val defaultHttpClient: OkHttpClient = config.client.let {
     client {
