476c2061097c8ddcc6e8e52061b0568a84ba7a26
Alpi <Alpieex@gmail.com>
2018-12-17 23:43:38 +0300

    Remove src for reverce PR

--- src/main/kotlin/io/github/rybalkinsd/kohttp/client/OkHttpClientExt.kt
+++
@@ -1,7 +0,0 @@
-package io.github.rybalkinsd.kohttp.client
-
-import okhttp3.OkHttpClient
-
-
-fun OkHttpClient.fork(block: ForkClientBuilder.() -> Unit) : OkHttpClient =
-    ClientBuilderImpl(this).apply(block).build()
