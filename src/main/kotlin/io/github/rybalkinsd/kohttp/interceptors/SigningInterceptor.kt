ce7636566c5375afd311296893c1e0d8cfbc156d
Gokul Chandrasekaran <gokulchandra93@gmail.com>
2019-01-20 14:52:25 +1100

    [Gokul] documents Interceptor

--- src/main/kotlin/io/github/rybalkinsd/kohttp/interceptors/SigningInterceptor.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/interceptors/SigningInterceptor.kt
@@ -5,6 +5,20 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
+/**
+ * Request Signing Interceptor
+ *
+ * Enables signing of query parameters.
+ *
+ * @param parameterName the name of the parameter with signed key
+ * @param signer function with HttpUrl as a receiver to sign the request parameter
+ *
+ * @see HttpUrl
+ *
+ * @since 0.8.0
+ * @author gokul
+ */
+
 class SigningInterceptor(private val parameterName: String, private val signer: HttpUrl.() -> String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
 
