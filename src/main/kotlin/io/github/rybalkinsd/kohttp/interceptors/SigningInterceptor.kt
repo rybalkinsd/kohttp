75c1d856ecfda539c441f9de729bffb664fc6d50
Gokul Chandrasekaran <gokulchandra93@gmail.com>
2019-01-21 23:28:32 +1100

    [Gokul] refactors code style

--- src/main/kotlin/io/github/rybalkinsd/kohttp/interceptors/SigningInterceptor.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/interceptors/SigningInterceptor.kt
@@ -12,27 +12,27 @@ import okhttp3.Response
  * @param parameterName the name of the parameter with signed key
  * @param signer function with HttpUrl as a receiver to sign the request parameter
  *
- * @see HttpUrl
+ * @see HttpUrl.Builder.addQueryParameter
  *
  * @since 0.8.0
  * @author gokul
  */
 
 class SigningInterceptor(private val parameterName: String, private val signer: HttpUrl.() -> String) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
+    override fun intercept(chain: Interceptor.Chain): Response =
 
-        return with(chain.request()) {
-            val signedKey = signer(url())
+            with(chain.request()) {
+                val signedKey = signer(url())
 
-            val requestUrl = with(url().newBuilder()) {
-                addQueryParameter(parameterName, signedKey)
-                build()
+                val requestUrl = with(url().newBuilder()) {
+                    addQueryParameter(parameterName, signedKey)
+                    build()
+                }
+
+                with(newBuilder()) {
+                    url(requestUrl)
+                    chain.proceed(build())
+                }
             }
-
-            with(newBuilder()) {
-                url(requestUrl)
-                chain.proceed(build())
-            }
-        }
-    }
 }
+
