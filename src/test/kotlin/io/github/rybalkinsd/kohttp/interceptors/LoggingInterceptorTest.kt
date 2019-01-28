cb7673439bbc41d566477bfee7d92de0b5e0b942
Gokul Chandrasekaran <gokulchandra93@gmail.com>
2019-01-28 15:30:07 +1100

    [Gokul] adds test for happy path

--- src/test/kotlin/io/github/rybalkinsd/kohttp/interceptors/LoggingInterceptorTest.kt
+++ src/test/kotlin/io/github/rybalkinsd/kohttp/interceptors/LoggingInterceptorTest.kt
@@ -1,4 +1,22 @@
 package io.github.rybalkinsd.kohttp.interceptors
 
+import io.github.rybalkinsd.kohttp.client.defaultHttpClient
+import io.github.rybalkinsd.kohttp.client.fork
+import io.github.rybalkinsd.kohttp.ext.httpGet
+import org.junit.Test
+import kotlin.test.assertTrue
+
 class LoggingInterceptorTest {
+    @Test
+    fun `calls passed logging method with message`() {
+        val client = defaultHttpClient.fork {
+            interceptors {
+                +LoggingInterceptor {
+                    assertTrue(it.isNotEmpty())
+                }
+            }
+        }
+
+        "https://postman-echo.com/get".httpGet(client = client)
+    }
 }
