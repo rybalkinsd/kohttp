701c4347d5f2c07c06325e08af6077bad7a02719
rybalkinsd <yan.brikl@gmail.com>
2019-04-17 08:48:26 +0300

    Iterator contract fix

--- src/main/kotlin/io/github/rybalkinsd/kohttp/ext/HeadersExt.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/ext/HeadersExt.kt
@@ -10,6 +10,10 @@ fun Headers.asSequence(): Sequence<Header> = Sequence {
     object : Iterator<Header> {
         private var cursor: Int = 0
         override fun hasNext() = cursor < size()
-        override fun next() = Header(name(cursor), value(cursor)).also { cursor++ }
+
+        override fun next(): Header {
+            if (!hasNext()) throw NoSuchElementException()
+            return Header(name(cursor), value(cursor)).also { cursor++ }
+        }
     }
 }
