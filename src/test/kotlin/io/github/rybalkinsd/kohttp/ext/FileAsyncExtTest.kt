eee8e2d36393059ae122e49d433436c8934cadbf
DeviantBadge <zhenya_vorobyov@mail.ru>
2019-05-27 18:07:45 +0300

    Add URL tests for extensions

--- src/test/kotlin/io/github/rybalkinsd/kohttp/ext/FileAsyncExtTest.kt
+++ src/test/kotlin/io/github/rybalkinsd/kohttp/ext/FileAsyncExtTest.kt
@@ -4,6 +4,7 @@ import io.github.rybalkinsd.kohttp.util.asJson
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import java.io.File
+import java.net.URL
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -13,7 +14,7 @@ import kotlin.test.assertTrue
 class FileAsyncExtTest {
 
     @Test
-    fun `upload file using File and string destination`() {
+    fun `async upload file using File and string destination`() {
         val file = File(this.javaClass.getResource("/cat.gif").toURI())
         val response = file.uploadAsync("http://postman-echo.com/post")
 
@@ -28,4 +29,19 @@ class FileAsyncExtTest {
         }
     }
 
+    @Test
+    fun `async upload file using File and URL destination`() {
+        val file = File(this.javaClass.getResource("/cat.gif").toURI())
+        val response = file.uploadAsync(URL("http://postman-echo.com/post"))
+
+        runBlocking {
+            response.await().use {
+                val parsedResponse = it.body()?.string().asJson()
+
+                assertEquals(200, it.code())
+                assertEquals(1046214, parsedResponse["headers"]["content-length"].asInt())
+                assertTrue { parsedResponse["headers"]["content-type"].asText().startsWith("multipart/mixed; boundary=") }
+            }
+        }
+    }
 }
