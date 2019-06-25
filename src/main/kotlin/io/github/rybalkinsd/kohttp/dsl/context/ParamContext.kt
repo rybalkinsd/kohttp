71ea4c3a6811b7d5bf78bbfadac9e93c4056c312
dmitry.tropanets <dtropanets@gmail.com>
2019-06-25 15:15:35 +0200

    change query container to list to support query lists.
    fixes an issue with query lists, e.g. "?id[]=1&id[]=2", where key would be duplicated and replaced in a map

--- src/main/kotlin/io/github/rybalkinsd/kohttp/dsl/context/ParamContext.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/dsl/context/ParamContext.kt
@@ -2,11 +2,12 @@ package io.github.rybalkinsd.kohttp.dsl.context
 
 @HttpDslMarker
 class ParamContext {
-    private val map: MutableMap<String, Any> = mutableMapOf()
+    private val list: MutableList<Pair<String, Any>> = mutableListOf()
 
     infix fun String.to(v: Any) {
-        map[this] = v
+        list.add(Pair(this, v))
     }
 
-    internal fun forEach(action: (k: String, v: Any) -> Unit) = map.forEach(action)
+    internal fun forEach(action: (k: String, v: Any) -> Unit) =
+        list.forEach { (k, v) -> action(k, v) }
 }
