94df9ba77fcd1fa351dd83319b27e0f3a4df8a36
dmitry.tropanets <dtropanets@gmail.com>
2019-06-25 15:16:22 +0200

    add tests for query

--- src/test/kotlin/io/github/rybalkinsd/kohttp/ext/HttpContextExtKtTest.kt
+++ src/test/kotlin/io/github/rybalkinsd/kohttp/ext/HttpContextExtKtTest.kt
@@ -1,6 +1,8 @@
 package io.github.rybalkinsd.kohttp.ext
 
+import io.github.rybalkinsd.kohttp.dsl.context.HttpContext
 import io.github.rybalkinsd.kohttp.dsl.context.HttpGetContext
+import okhttp3.HttpUrl
 import org.junit.Test
 import java.net.MalformedURLException
 import java.net.URL
@@ -11,11 +13,12 @@ class HttpContextExtKtTest {
 
     @Test
     fun `full url test`() {
-        val context = HttpGetContext().apply { url("https://www.example.org:8080/path") }
-        assertEquals("https", context.scheme)
-        assertEquals("www.example.org", context.host)
-        assertEquals("/path", context.path)
-        assertEquals(8080, context.port)
+        val url = httpUrlFor { url("https://www.example.org:8080/path?foo=bar") }
+        assertEquals("https", url.scheme())
+        assertEquals("www.example.org", url.host())
+        assertEquals("/path", url.encodedPath())
+        assertEquals(8080, url.port())
+        assertEquals("foo=bar", url.query())
     }
 
     @Test
@@ -44,18 +47,58 @@ class HttpContextExtKtTest {
     // expecting NPE for b/c of protocol
     @Test(expected = NullPointerException::class)
     fun `null protocol url`() {
-        HttpGetContext().apply { url(URL(null, null,0, "cat.gif")) }
+        HttpGetContext().apply { url(URL(null, null, 0, "cat.gif")) }
     }
 
 
     @Test(expected = IllegalArgumentException::class)
     fun `not http or https protocol url`() {
-        HttpGetContext().apply { url(URL("file", null,0, "cat.gif")) }
+        HttpGetContext().apply { url(URL("file", null, 0, "cat.gif")) }
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `null host url`() {
-        HttpGetContext().apply { url(URL("https", null,0, "cat.gif")) }
+        HttpGetContext().apply { url(URL("https", null, 0, "cat.gif")) }
     }
 
+    @Test
+    fun `empty query`() {
+        val url = httpUrlFor { url("https://www.example.org/?") }
+        assertEquals(null, url.query())
+    }
+
+    @Test
+    fun `query with malformed path`() {
+        val url = httpUrlFor { url("https://www.example.org/?a=b") }
+        assertEquals("a=b", url.query())
+    }
+
+    @Test
+    fun `query added from 2 sources`() {
+        val url = httpUrlFor {
+            url("https://www.example.org/path?a=xxx&a=")
+            param {
+                "a" to "yyy"
+            }
+        }
+        assertEquals("a=xxx&a=&a=yyy", url.query())
+    }
+
+    @Test
+    fun `duplicate query keys`() {
+        val url = httpUrlFor { url("https://www.example.org/path?id[]=1&id[]=2") }
+        assertEquals("id[]=1&id[]=2", url.query())
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `query without value`() {
+        HttpGetContext().apply { url("https://www.example.org/path?foo") }
+    }
+
+    private fun httpUrlFor(init: HttpContext.() -> Unit): HttpUrl =
+        HttpGetContext()
+            .run {
+                init()
+                makeUrl().build()
+            }
 }
