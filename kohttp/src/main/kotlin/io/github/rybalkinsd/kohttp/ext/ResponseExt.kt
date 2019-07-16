1889895631ddf1f873cf4af0676b51b4a9ca7ee9
Sergei Rybalkin <yan.brikl@gmail.com>
2019-07-16 18:34:20 +0300

    Response body ext improvements #132 (#133)
    * 1. closing body
    2. added explicit types
    3. added docs
    
    * changed patch configuration for codcov
    
    * Update kohttp/src/main/kotlin/io/github/rybalkinsd/kohttp/ext/ResponseExt.kt
    
    Co-Authored-By: DeviantBadge <zhenya_vorobyov@mail.ru>
    
    * Update kohttp/src/main/kotlin/io/github/rybalkinsd/kohttp/ext/ResponseExt.kt
    
    Co-Authored-By: DeviantBadge <zhenya_vorobyov@mail.ru>

--- kohttp/src/main/kotlin/io/github/rybalkinsd/kohttp/ext/ResponseExt.kt
+++ kohttp/src/main/kotlin/io/github/rybalkinsd/kohttp/ext/ResponseExt.kt
@@ -1,22 +1,29 @@
 package io.github.rybalkinsd.kohttp.ext
 
 import okhttp3.Response
-
+import java.io.InputStream
 
 /**
  * Returns Response Body as String.
  *
  * @return Response body as a `String?`.
+ * <p>
+ * Note: This function will close `ResponseBody`
+ * </p>
  * @since 0.9.0
- * @author gokul
+ * @author gokul, sergey
  */
-fun Response.asString() = body()?.string()
+fun Response.asString(): String? = body()?.use { it.string() }
 
 /**
- *  Returns Response Body as a Stream.
+ * Returns Response Body as a Stream.
+ *
  *
  * @return Response body as a `InputStream?`.
+ * <p>
+ * Note: Response stream must be closed after use
+ * </p>
  * @since 0.9.0
  * @author gokul
  */
-fun Response.asStream() = body()?.byteStream()
+fun Response.asStream(): InputStream? = body()?.byteStream()
