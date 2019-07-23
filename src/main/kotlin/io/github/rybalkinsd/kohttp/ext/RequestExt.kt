eb44f53fefc83825813965e6ed212a8bee5a13ac
doyaaaaaken <kenta.koyama.biz@gmail.com>
2019-07-23 21:23:22 +0900

    refactor code

--- src/main/kotlin/io/github/rybalkinsd/kohttp/ext/RequestExt.kt
+++ src/main/kotlin/io/github/rybalkinsd/kohttp/ext/RequestExt.kt
@@ -13,21 +13,16 @@ internal fun Request.buildCurlCommand(): String {
     var compressed = false
     //TODO: test
     return StringBuilder().apply {
-        append("curl")
-        append(" -X ${method()}")
+        append("curl -X ${method()}")
 
         //headers
         val headers = headers()
         for (i in 0 until headers.size()) {
             val name = headers.name(i)
             var value = headers.value(i)
-
-            val start = 0
-            val end = value.length - 1
-            if (value[start] == '"' && value[end] == '"') {
-                value = "\\\"" + value.substring(1, end) + "\\\""
+            if (value[0] == '"' && value[value.length - 1] == '"') {
+                value = "\\\"" + value.substring(1, value.length - 1) + "\\\""
             }
-
             if ("Accept-Encoding".equals(name, ignoreCase = true) && "gzip".equals(value, ignoreCase = true)) {
                 compressed = true
             }
@@ -37,9 +32,8 @@ internal fun Request.buildCurlCommand(): String {
         //body
         body()?.let {
             val buffer = Buffer().apply { it.writeTo(this) }
-            val UTF8 = Charset.forName("UTF-8")
-            val charset = it.contentType()?.charset(UTF8) ?: UTF8
-            // try to keep to a single line and use a subshell to preserve any line breaks
+            val utf8 = Charset.forName("UTF-8")
+            val charset = it.contentType()?.charset(utf8) ?: utf8
             append(" --data $'" + buffer.readString(charset).replace("\n", "\\n") + "'")
         }
 
@@ -49,3 +43,4 @@ internal fun Request.buildCurlCommand(): String {
         append(" \"${url()}\"")
     }.toString()
 }
+
