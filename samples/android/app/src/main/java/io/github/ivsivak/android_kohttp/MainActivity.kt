88765ee28860e5d7483a2254f98d00fa477781d3
ivsivak <ivsivak@protonmail.ch>
2019-08-01 09:05:09 +0300

    Init android sample

---
+++ samples/android/app/src/main/java/io/github/ivsivak/android_kohttp/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.ivsivak.android_kohttp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
