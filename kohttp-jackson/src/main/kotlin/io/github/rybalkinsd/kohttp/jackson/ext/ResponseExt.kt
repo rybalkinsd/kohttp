7375447659a550b1debe6224735ae4426a3f6a69
rybalkinsd <yan.brikl@gmail.com>
2019-08-16 14:11:47 +0100

    Add mapper to toJsonOrNull and toType function parameters

--- kohttp-jackson/src/main/kotlin/io/github/rybalkinsd/kohttp/jackson/ext/ResponseExt.kt
+++ kohttp-jackson/src/main/kotlin/io/github/rybalkinsd/kohttp/jackson/ext/ResponseExt.kt
@@ -3,30 +3,24 @@ package io.github.rybalkinsd.kohttp.jackson.ext
 import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.NullNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import okhttp3.Response
 
 
-class DeserializationException(cause: String) : Exception(cause)
-
-val default: ObjectMapper by lazy { jacksonObjectMapper() }
-
-
 /**
- * Returns Response Body deserialized as [JsonNode] for valid content.
- * Returns [NullNode] if body has no content to bind
+ * Deserialize [ResponseBody] as [JsonNode].
  *
+ * @param mapper is a [ObjectMapper] that gonna be used to deserialize [JsonNode]
  * @return [JsonNode]
  *
- * @throws [DeserializationException] if `RequestBody` is null
+ * @throws [DeserializationException] if there is no content to bind.
  * @throws [JsonParseException] if underlying input contains invalid content
  *    of type {@link JsonParser} supports (JSON for default case)
  * @throws [IOException] If a low-level I/O problem (missing input, network error) occurs
  *
  * @since 0.9.0
- * @author gokul
+ * @author gokul, sergey
  */
 fun Response.toJson(mapper: ObjectMapper = default): JsonNode {
     val content = body()?.string()
@@ -38,9 +32,10 @@ fun Response.toJson(mapper: ObjectMapper = default): JsonNode {
 }
 
 /**
- * Returns Response Body as [JsonNode?].
+ * Deserialize [ResponseBody] as [JsonNode?].
  * Returns null on invalid content
  *
+ * @param mapper is a [ObjectMapper] that gonna be used to deserialize [JsonNode]
  * @return [JsonNode] and on invalid Json content
  *
  * @throws [IOException] If a low-level I/O problem (missing input, network error) occurs
@@ -48,29 +43,34 @@ fun Response.toJson(mapper: ObjectMapper = default): JsonNode {
  * @since 0.11.0
  * @author sergey
  */
-fun Response.toJsonOrNull(): JsonNode? = try {
-    toJson()
+fun Response.toJsonOrNull(mapper: ObjectMapper = default): JsonNode? = try {
+    toJson(mapper)
 } catch (ignored: DeserializationException) {
     null
 } catch (ignored: JsonParseException) {
     null
 }
 
-
 /**
- * Returns deserialized response body as [T]
+ * Deserialize response body as [T]
  *
- * @return [T]
+ * @param mapper is a [ObjectMapper] that gonna be used to deserialize [JsonNode]
+ * @return [T]?
+ *
  * @throws [IOException] If a low-level I/O problem (missing input, network error) occurs
  * @throws [JsonParseException] if underlying input contains invalid content
  *    of type {@link JsonParser} supports (JSON for default case)
  * @throws [JsonMappingException] if the input JSON structure does not match structure
  *   expected for result type (or has other mismatch issues)
  *
- * @since TODO()
+ * @since 0.11.0
  * @author sergey
  */
-inline fun <reified T : Any> Response.toType(): T? = with(body()?.string()) {
+inline fun <reified T : Any> Response.toType(mapper: ObjectMapper = default): T? = with(body()?.string()) {
     if (isNullOrBlank()) null
-    else default.readValue<T>(this!!)
+    else mapper.readValue<T>(this!!)
 }
+
+val default: ObjectMapper by lazy { jacksonObjectMapper() }
+
+class DeserializationException(cause: String) : Exception(cause)
