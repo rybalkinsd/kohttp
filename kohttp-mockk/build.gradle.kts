9e42b027ae41ea6ae2ad277660764b43f72bc371
Sergei Rybalkin <sergeirybalkin@fb.com>
2020-05-14 12:20:12 +0100

    Matchers

--- kohttp-mockk/build.gradle.kts
+++ kohttp-mockk/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     api(project(":kohttp"))
 
-    implementation("io.mockk:mockk:1.9.3")
+    implementation("io.mockk:mockk:1.10.0")
 
     testImplementation(kotlin("test-junit"))
     testImplementation("org.assertj:assertj-core:3.12.2")
