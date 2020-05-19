dc97f5fe8ebc19dbd3970d60615b220ff43b2429
Hakan Altindag <hakangoudberg@hotmail.com>
2020-05-19 23:43:28 +0200

    Revert "Moved body field to base class and added body validations for HEAD and GET request"
    This reverts commit 92cd43af

--- kohttp-test/src/test/kotlin/io/github/rybalkinsd/kohttp/dsl/HttpDslKtTest.kt
+++ kohttp-test/src/test/kotlin/io/github/rybalkinsd/kohttp/dsl/HttpDslKtTest.kt
@@ -4,19 +4,16 @@ import io.github.rybalkinsd.kohttp.assertContainsAtLeast
 import io.github.rybalkinsd.kohttp.assertContainsExactly
 import io.github.rybalkinsd.kohttp.client.defaultHttpClient
 import io.github.rybalkinsd.kohttp.client.fork
-import io.github.rybalkinsd.kohttp.dsl.context.HttpContext
-import io.github.rybalkinsd.kohttp.dsl.context.Method
+import io.github.rybalkinsd.kohttp.dsl.context.*
 import io.github.rybalkinsd.kohttp.interceptors.logging.HttpLoggingInterceptor
 import io.github.rybalkinsd.kohttp.jackson.ext.toJson
-import io.github.rybalkinsd.kohttp.util.json
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.*
 import org.junit.Test
 
 class HttpDslKtTest {
 
     @Test
-    fun `post request with param and header and body`() {
+    fun `post request with HttpPostContext`() {
         val expectedHeader = mapOf(
             "one" to "42",
             "cookie" to "aaa=bbb; ccc=42"
@@ -37,7 +34,7 @@ class HttpDslKtTest {
             }
         }
 
-        http(client, method = Method.POST, init = fun HttpContext.() {
+        http(client, method = Method.POST, init = fun HttpPostContext.() {
             host = "postman-echo.com"
             path = "/post"
 
@@ -69,47 +66,38 @@ class HttpDslKtTest {
     }
 
     @Test
-    fun `get request with param and header`() {
-        val variable = 123L
+    fun `post request with HttpContext`() {
         val expectedHeader = mapOf(
                 "one" to "42",
-                "two" to variable.toString(),
-                "three" to """{"a":$variable,"b":{"b1":"512"},"c":[1,2.0,3]}""",
                 "cookie" to "aaa=bbb; ccc=42"
         )
 
         val expectedParams = mapOf(
-                "text" to "iphone",
-                "lr" to "213"
+                "arg" to "iphone"
         )
 
-        val response = http(method = Method.GET) {
+        val client = defaultHttpClient.fork {
+            interceptors {
+                +HttpLoggingInterceptor()
+            }
+        }
+
+        http(client, method = Method.POST, init = fun HttpContext.() {
             host = "postman-echo.com"
-            path = "/get"
+            path = "/post"
+
+            param {
+                "arg" to "iphone"
+            }
 
             header {
                 "one" to 42
-                "two" to variable
-                "three" to json {
-                    "a" to variable
-                    "b" to json {
-                        "b1" to "512"
-                    }
-                    "c" to listOf(1, 2.0, 3)
-                }
-
                 cookie {
                     "aaa" to "bbb"
                     "ccc" to 42
                 }
             }
-
-            param {
-                "text" to "iphone"
-                "lr" to 213
-            }
-        }
-        response.use {
+        }).use {
             val parsedResponse = it.toJson()
             assertContainsAtLeast(expectedHeader, parsedResponse["headers"])
             assertContainsExactly(expectedParams, parsedResponse["args"])
@@ -118,49 +106,87 @@ class HttpDslKtTest {
     }
 
     @Test
-    fun `get request with body throws exception`() {
-        val variable = 123L
+    fun `post request with HttpGetContext should throw exception`() {
+        val client = defaultHttpClient.fork {
+            interceptors {
+                +HttpLoggingInterceptor()
+            }
+        }
 
-        assertThatCode {
-            http(method = Method.GET) {
+        assertThatThrownBy {
+            http(client, method = Method.POST, init = fun HttpGetContext.() {
                 host = "postman-echo.com"
-                path = "/get"
+                path = "/post"
+
+                param {
+                    "arg" to "iphone"
+                }
 
                 header {
                     "one" to 42
-                    "two" to variable
-                    "three" to json {
-                        "a" to variable
-                        "b" to json {
-                            "b1" to "512"
-                        }
-                        "c" to listOf(1, 2.0, 3)
-                    }
-
                     cookie {
                         "aaa" to "bbb"
                         "ccc" to 42
                     }
                 }
+            })
+        }.isInstanceOf(ClassCastException::class.java)
+    }
+
+    @Test
+    fun `get request with HttpGetContext`() {
+        val response = http(method = Method.GET, init = fun HttpGetContext.() {
+            host = "yandex.ru"
+            path = "/search"
+            port = 80
+
+            param {
+                "text" to "iphone"
+                "lr" to 213
+            }
+        })
+
+        response.use {
+            assertThat(it.code()).isEqualTo(200)
+        }
+    }
+
+    @Test
+    fun `get request with HttpContext`() {
+        val response = http(method = Method.GET, init = fun HttpContext.() {
+            host = "yandex.ru"
+            path = "/search"
+            port = 80
+
+            param {
+                "text" to "iphone"
+                "lr" to 213
+            }
+        })
+
+        response.use {
+            assertThat(it.code()).isEqualTo(200)
+        }
+    }
+
+    @Test
+    fun `get request with HttpPostContext should throw exception`() {
+        assertThatThrownBy {
+            http(method = Method.GET, init = fun HttpPostContext.() {
+                host = "yandex.ru"
+                path = "/search"
+                port = 80
 
                 param {
                     "text" to "iphone"
                     "lr" to 213
                 }
-
-                body {
-                    form {
-                        "login" to "user"
-                        "email" to "john.doe@gmail.com"
-                    }
-                }
-            }
-        }.isInstanceOf(UnsupportedOperationException::class.java)
-         .hasMessage("Request body is not supported for [GET] Method.")
+            })
+        }.isInstanceOf(ClassCastException::class.java)
     }
 
     @Test
-    fun `delete request with param and header`() {
+    fun `delete request with HttpDeleteContext`() {
         val expectedHeader = mapOf(
                 "one" to "42",
                 "cookie" to "aaa=bbb; ccc=42"
@@ -175,7 +201,7 @@ class HttpDslKtTest {
                 "email" to "john.doe@gmail.com"
         )
 
-        http(method = Method.DELETE, init = fun HttpContext.() {
+        http(method = Method.DELETE, init = fun HttpDeleteContext.() {
             host = "postman-echo.com"
             path = "/delete"
 
@@ -204,6 +230,84 @@ class HttpDslKtTest {
             assertContainsExactly(expectedForm, parsedResponse["form"])
             assertThat(it.code()).isEqualTo(200)
         }
+    }
+
+    @Test
+    fun `delete request with HttpContext`() {
+        val expectedHeader = mapOf(
+                "one" to "42",
+                "cookie" to "aaa=bbb; ccc=42"
+        )
+
+        val expectedParams = mapOf(
+                "arg" to "iphone"
+        )
+
+        http(method = Method.DELETE, init = fun HttpContext.() {
+            host = "postman-echo.com"
+            path = "/delete"
+
+            param {
+                "arg" to "iphone"
+            }
+
+            header {
+                "one" to 42
+                cookie {
+                    "aaa" to "bbb"
+                    "ccc" to 42
+                }
+            }
+        }).use {
+            val parsedResponse = it.toJson()
+            assertContainsAtLeast(expectedHeader, parsedResponse["headers"])
+            assertContainsExactly(expectedParams, parsedResponse["args"])
+            assertThat(it.code()).isEqualTo(200)
+        }
+    }
+
+    @Test
+    fun `delete request with HttpPatchContext should throw exception`() {
+        assertThatThrownBy {
+            http(method = Method.DELETE, init = fun HttpPatchContext.() {
+                host = "postman-echo.com"
+                path = "/delete"
+
+                param {
+                    "arg" to "iphone"
+                }
+
+                header {
+                    "one" to 42
+                    cookie {
+                        "aaa" to "bbb"
+                        "ccc" to 42
+                    }
+                }
+            })
+        }.isInstanceOf(ClassCastException::class.java)
+    }
+
+    @Test
+    fun `delete request with HttpPutContext should throw exception`() {
+        assertThatThrownBy {
+            http(method = Method.DELETE, init = fun HttpPutContext.() {
+                host = "postman-echo.com"
+                path = "/delete"
+
+                param {
+                    "arg" to "iphone"
+                }
+
+                header {
+                    "one" to 42
+                    cookie {
+                        "aaa" to "bbb"
+                        "ccc" to 42
+                    }
+                }
+            })
+        }.isInstanceOf(ClassCastException::class.java)
     }
 
 }
